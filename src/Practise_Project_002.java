2da3b387f547c2703f5e2b5542583d21a54d1bc5
Mayuri1920 <mayurigarad2008@gmail.com>
2025-09-11 16:03:09 +0200

    My second and Third practise java program

---
+++ src/Practise_Project_002.java
@@ -0,0 +1,5 @@
+public class Practise_Project_002 {
+    public static void main(String[] args) {
+        System.out.println("This is my second practise java program");
+    }
+}
---
+++ src/Practise_Project_003.java
@@ -0,0 +1,5 @@
+public class Practise_Project_003 {
+    public static void main(String[] args) {
+        System.out.println("This Is My Third Practise Java Project");
+    }
+}
