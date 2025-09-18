1aee2afd7ada31237422d8e1c914b625cfcf7498
Mayuri1920 <mayurigarad2008@gmail.com>
2025-09-18 22:10:37 +0200

    Practise of primitive  data type and variables

---
+++ src/Practise_datatype_and_variable.java
@@ -0,0 +1,14 @@
+public class Practise_datatype_and_variable {
+    public static void main(String[] args) {
+        boolean ismayuristudying = true;
+        boolean Java_is_easy = false;
+        byte Mayuri_age = 28    ;
+        short _worldcountries = 195;
+        int age = 129;
+        System.out.println(age);
+        System.out.println(_worldcountries);
+        System.out.println(Mayuri_age);
+        System.out.println(Java_is_easy);
+        System.out.println(ismayuristudying);
+    }
+}
